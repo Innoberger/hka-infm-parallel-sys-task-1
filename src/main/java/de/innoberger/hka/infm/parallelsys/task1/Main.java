251097c073813b96f5cdcdeec710fb78abda989e
Innoberger <fynn.arnold@pm.me>
2022-11-22 12:37:36 +0100

    improved time periods

--- src/main/java/de/innoberger/hka/infm/parallelsys/task1/Main.java
+++ src/main/java/de/innoberger/hka/infm/parallelsys/task1/Main.java
@@ -2,6 +2,7 @@ package de.innoberger.hka.infm.parallelsys.task1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
@@ -10,7 +11,31 @@ public class Main {
         int innenraumreinigungCapacity = 4;
 
         WaschPark waschPark = new WaschPark(waschstraßenCapacity, innenraumreinigungCapacity);
-        List<Auto> autos = new ArrayList<Auto>();
+        Random random = new Random();
+
+        // NACHMITTAG
+        timePeriod(random, "Nachmittag", 3, 5);
+
+        // RUSH HOUR
+        timePeriod(random, "Rush Hour", 4, 7);
+
+        // ABEND
+        timePeriod(random, "Abend", 3, 5);
+    }
+
+    public static void timePeriod(Random random, String name, int minAutos, int maxAutos) {
+        System.out.println("--- " + name + " hat begonnen");
+
+        for (int i = 0; i < 12; i++) {
+            int autoAmount = random.nextInt(3, maxAutos + 1);
+            System.out.println(name + " Minute " + (i * 5) + ": " + autoAmount + " Autos kommen vorbei");
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ie) {}
+        }
+
+        System.out.println(name + " ist vorbei");
     }
 
 }
